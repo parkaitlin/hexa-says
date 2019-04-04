0c2111bf7e4f29712c37bf0a687110e03b91454d
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-04-04 14:27:04 -0700

    Commit 4 - createfunction for first round

--- js/app.js
+++ js/app.js
@@ -1,23 +1,51 @@
 console.log('linked')
-
+// test
 array = [];
 
+// music starts immediately
+
+
 document.querySelector('.hexagons').addEventListener('click', (e)=>{
-    console.log(e.target.id);
-    array.push(e.target.id)
+    console.log(e.target.id); // test to find value of target
+    array.push(e.target.id); 
+    document.getElementById(e.target.id).style.backgroundColor='blue'
     console.log(array);
 })
 
 const countdown = ()=>{
     document.getElementsByClassName('.overlay').style
-    // hide start screen
+    // hide start screen (display to none)
     // countdown 3-2-1
-    // 2s pause
-    // start music
+    // unblur game screen
+    // 2s pause setTimeout
+    // startRoundOne()
 }
 
-const startRoundOne = ()=>{
+let i = 0;
+const playSequence = ()=>{
+setTimeout(()=>{
+    const lightUp = document.getElementById(game.hexArray[i])
+    const lightOff = document.getElementById(game.hexArray[i])
+    lightUp.style.backgroundColor = "pink";
+    setTimeout(()=>{
+    lightOff.style.backgroundColor = "green";
+    }, 1500); 
+if(i === game.hexArray.length-1){
+    return
+} else if(i < game.hexArray.length){
+    playSequence();
+}
+i++
+},2000)
+}
 
+const startFirstRound = ()=>{
+for(let i = 1; i < 5; i++){
+    let randomNum = Math.floor(Math.random() * 6 + 1);
+    game.hexArray.push(`hex${randomNum}`);
+    console.log(game.hexArray); // test 
+}
+playSequence();
 }
 
 const startNextRound = ()=>{
@@ -39,32 +67,13 @@ const game = {
     playerArray: [],
     round: 0
 }
-// initiateGame function
-    // startGame = true
-    // player = new Player
-    // countdown function
 
-// game object:
-    //startGame:false 
-    //initiateGame function
-    //Player: {}
-    //hexaArray: []
-    //playerArray: []
+document.querySelector('.start').addEventListener('click', ()=>{
+    console.log('started game') // test for click
+    // initiateGame();
+})
 
-// countdown function
-    //
-    // start background music
 
-// event listener for start button <-- background should be blurred
-// initiateGame function <-- background should be blurred
-// countdown function <-- background should be blurred
-// game screen fadesin <-- first sequence starts
-
-// randomizer value
-    // const randomHexagon = Math.floor(Math.random() * 14)
-// first sequence randomly lights up 4 hexagons (one at a time)
-    // after each hexagon lights up the id of the hexagon is pushed into the hexaArray
-    // THIS IS FIRST SEQUENCE FUNCTION
 // somehow prompt (don't use prompt) player to copy sequence
 // attach a event listenter for clicks on the hexagons
     // for each click --> push the e.target.id to the player array
@@ -96,3 +105,43 @@ const game = {
 
 //sequencing needs to build off of the existing array 
     // and also repeat the whole array again each round
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// initiateGame function
+    // startGame = true
+    // player = new Player
+    // countdown function
+
+// game object:
+    //startGame:false 
+    //initiateGame function
+    //Player: {}
+    //hexaArray: []
+    //playerArray: []
+
+// countdown function
+    //
+    // start background music
+
+// event listener for start button <-- background should be blurred
+// initiateGame function <-- background should be blurred
+// countdown function <-- background should be blurred
+// game screen fadesin <-- first sequence starts
+
+// randomizer value
+    // const randomHexagon = Math.floor(Math.random() * 14)
+// first sequence randomly lights up 4 hexagons (one at a time)
+// after each hexagon lights up the id of the hexagon is pushed into the hexaArray
+// THIS IS FIRST SEQUENCE FUNCTION
--- js/player.js
+++ js/player.js
@@ -4,8 +4,15 @@ class Player {
     constructor(){
         this.score = 0;
     }
+    selectHex(){
+        // place some animations (when the player clicks on something color of the hexagon changes and their is some animation)
+    }
     checkForMatch(){
-        
+        for(let i = 0; i < game.playerArray.length; i++){
+            if(game.playerArray[i] !== game.hexArray[i]){
+                //game over
+            }
+        }
     }
     nextRound(){
 
